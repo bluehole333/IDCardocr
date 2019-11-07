639817bcd957b7f088b82c3f941ca0bd687b2379
bluehole333 <bluehole333@users.noreply.github.com>
2019-11-07 12:40:06 +0800

    idnumber验证

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import datetime
+
 from ocr import Ocr
 
 if __name__ == "__main__":
+    starttime = datetime.datetime.now()
     ocr = Ocr('test_img/test2.jpg')
     ocr.go()
+    endtime = datetime.datetime.now()
+    print((endtime - starttime).seconds)
